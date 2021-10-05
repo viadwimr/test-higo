66ce8e74dbe2c10e97615739e3e878073d0ce0ba
viadwimr <viadwimr@github.com>
2021-10-05 12:25:17 +0700

    add detail device

--- cypress/integration/device.js
+++ cypress/integration/device.js
@@ -14,7 +14,61 @@ describe('Device', () => {
 
   it('Filter sektor', () => {
     cy.wait(3000);
-    cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click();
+    cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
     cy.contains('Enrober', timeout).click();
+  });
+
+  describe('Detail Device', () => {
+    describe('Edit Device', () => {
+      //Edit Device
+    });
+      
+    describe('Setting Kalibrasi', () => {
+      it('Validasi Input dan Simpan', () => {
+        cy.wait(3000);
+        cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
+        cy.contains('Ball Mill', timeout).click();
+        cy.get(':nth-child(1) > a > .sector-card', timeout).click();
+        cy.get(':nth-child(2) > .ant-row > :nth-child(2) > .ant-dropdown-trigger', timeout).click();
+        cy.contains('Edit Kalibrasi', timeout).click();
+        cy.get('.DetailCondition__InputNum-z5ehht-1', timeout).clear().type('abc').should('have.value', '');
+        cy.get('.DetailCondition__InputNum-z5ehht-1', timeout).clear().type('3,9').should('have.value', '39');
+        cy.get('.DetailCondition__InputNum-z5ehht-1', timeout).clear().type('44').should('have.value', '44');
+        cy.get('.ant-row-end > .Button__BaseButton-sc-1hmbtsr-0', timeout).click();
+      });
+
+      it('Cek Data Kalibrasi', () => {
+        cy.wait(3000);
+        cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
+        cy.contains('Ball Mill', timeout).click();
+        cy.get(':nth-child(1) > a > .sector-card', timeout).click();
+        cy.get(':nth-child(2) > .ant-row > :nth-child(2) > .ant-dropdown-trigger', timeout).click();
+        cy.contains('Edit Kalibrasi', timeout).click();
+        cy.get('.DetailCondition__InputNum-z5ehht-1', timeout).should('have.value', '44');
+        const todaysDate = Cypress.moment().format('DD/MM/YYYY')
+        cy.get('p', timeout).should('have.contain', 'Terakhir Diubah: ' + todaysDate);
+        cy.get('.ant-modal-close-x').click();
+      });
+    });
+
+    describe('Export Chart', () => {
+      it('Export JPG', () => {
+        cy.wait(3000);
+        cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
+        cy.contains('Ball Mill', timeout).click();
+        cy.get(':nth-child(1) > a > .sector-card', timeout).click();
+        cy.get(':nth-child(2) > .ant-row > :nth-child(2) > .ant-dropdown-trigger', timeout).click();
+        cy.contains('Unduh sebagai JPG', timeout).click();
+      });
+      
+      it('Export PDF', () => {
+        cy.wait(3000);
+        cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
+        cy.contains('Ball Mill', timeout).click();
+        cy.get(':nth-child(1) > a > .sector-card', timeout).click();
+        cy.get(':nth-child(2) > .ant-row > :nth-child(2) > .ant-dropdown-trigger', timeout).click();
+        cy.contains('Unduh sebagai PDF', timeout).click();
+      });
+    });
   });
 });
