3db6a52d7904ba42260d444110395885f20297c6
viadwimr <viadwimr@github.com>
2021-11-07 00:03:06 +0700

    add sorting and update data

--- cypress/integration/indicator.js
+++ cypress/integration/indicator.js
@@ -24,52 +24,66 @@ describe('Indicator', () => {
     });
   });
 
+  describe('Simpan Nilai Indicator', () => {
+    before(() => {
+      cy.get('[title="Indicator"] > .ant-menu-title-content > a', timeout).click();
+    });
+  
+    it('Arus Listrik', () => {
+      cy.get(':nth-child(1) > label', timeout).click();
+      cy.get('#indikator_form_min_valid', timeout).clear().type('37');
+      cy.get('#indikator_form_max_valid', timeout).clear({ force: true }).type('94');
+      cy.get('.IndicatorPage__BtnSubmit-sc-1da0oss-7', timeout).click({ force: true });
+    });
+  
+    it('Kelembaban', () => {
+      cy.get(':nth-child(7) > label', timeout).click({ force: true });
+      cy.get('#indikator_form_min_valid', timeout).clear({ force: true }).type('79', { force: true });
+      cy.get('#indikator_form_max_valid', timeout).clear({ force: true }).type('800', { force: true });
+      cy.get('.IndicatorPage__BtnSubmit-sc-1da0oss-7', timeout).click({ force: true });
+    })
+  });
+  
+  describe('Cek Data Indicator', () => {
+    before(() => {
+      cy.get('[title="Threshold"] > .ant-menu-title-content > a', timeout).click({ force: true });
+      cy.wait(3000);
+      cy.get('[title="Indicator"] > .ant-menu-title-content > a', timeout).click({ force: true });
+    });
+  
+    it('Arus Listrik', () => {
+      cy.get(':nth-child(1) > label', timeout).click({ force: true });
+      cy.get('#indikator_form_sensor_name', timeout).should('have.value', 'Arus Listrik');
+      cy.get('#indikator_form_sensor_unit', timeout).should('have.value', 'A');
+      cy.get('#indikator_form_min_valid', timeout).should('have.value', '37');
+      cy.get('#indikator_form_max_valid', timeout).should('have.value', '94');
+    });
+  
+    it('Kelembaban', () => {
+      cy.get(':nth-child(7) > label', timeout).click({ force: true });
+      cy.get('#indikator_form_sensor_name', timeout).should('have.value', 'Kelembaban');
+      cy.get('#indikator_form_sensor_unit', timeout).should('have.value', '%');
+      cy.get('#indikator_form_min_valid', timeout).should('have.value', '79');
+      cy.get('#indikator_form_max_valid', timeout).should('have.value', '800');
+    });
+  });
+  
+  describe('Sorting Data Indicator', () => {
+    before(() => {
+      cy.get('[title="Threshold"] > .ant-menu-title-content > a', timeout).click({ force: true });
+      cy.wait(3000);
+      cy.get('[title="Indicator"] > .ant-menu-title-content > a', timeout).click({ force: true });
+    });
+  
+    it('Sorting List Indicator', () => {
+      cy.contains('Arus Listrik', timeout).should('be.visible');
+      cy.contains('Daya', timeout).should('be.visible');
+      cy.contains('Frequency', timeout).should('be.visible');
+      cy.contains('Intensitas Cahaya', timeout).should('be.visible');
+      cy.contains('Kalium', timeout).should('be.visible');
+      cy.contains('Kecepatan Angin', timeout).should('be.visible');
+    });
+  });
 });
 
-describe('Simpan Nilai Indicator', () => {
-  before(() => {
-    cy.get('[title="Indicator"] > .ant-menu-title-content > a', timeout).click();
-  });
 
-  //Simpan Nilai Min Max TimeUpdate
-  it('TimeUpdate', () => {
-    cy.get(':nth-child(1) > label', timeout).click();
-    cy.get('#indikator_form_min_valid', timeout).clear().type('37');
-    cy.get('#indikator_form_max_valid', timeout).clear({ force: true }).type('94');
-    cy.get('.IndicatorPage__BtnSubmit-sc-1da0oss-7', timeout).click({ force: true });
-  });
-
-  //Simpan Nilai Min Max Daya
-  it('Daya', () => {
-    cy.get(':nth-child(7) > label', timeout).click({ force: true });
-    cy.get('#indikator_form_min_valid', timeout).clear({ force: true }).type('79', { force: true });
-    cy.get('#indikator_form_max_valid', timeout).clear({ force: true }).type('800', { force: true });
-    cy.get('.IndicatorPage__BtnSubmit-sc-1da0oss-7', timeout).click({ force: true });
-  })
-});
-
-describe('Cek Data Indicator', () => {
-  before(() => {
-    cy.get('[title="Threshold"] > .ant-menu-title-content > a', timeout).click({ force: true });
-    cy.wait(3000);
-    cy.get('[title="Indicator"] > .ant-menu-title-content > a', timeout).click({ force: true });
-  });
-
-  //Cek Time
-  it('Time', () => {
-    cy.get(':nth-child(1) > label', timeout).click({ force: true });
-    cy.get('#indikator_form_sensor_name', timeout).should('have.value', 'Time');
-    cy.get('#indikator_form_sensor_unit', timeout).should('have.value', 's');
-    cy.get('#indikator_form_min_valid', timeout).should('have.value', '37');
-    cy.get('#indikator_form_max_valid', timeout).should('have.value', '94');
-  });
-
-  //Cek Daya
-  it('Daya', () => {
-    cy.get(':nth-child(7) > label', timeout).click({ force: true });
-    cy.get('#indikator_form_sensor_name', timeout).should('have.value', 'Daya');
-    cy.get('#indikator_form_sensor_unit', timeout).should('have.value', 'kW');
-    cy.get('#indikator_form_min_valid', timeout).should('have.value', '79');
-    cy.get('#indikator_form_max_valid', timeout).should('have.value', '800');
-  });
-});
