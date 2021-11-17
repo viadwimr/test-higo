ae00af10e785ca2301faf1491cb08ef26992ed7b
Renal <renal.prahardis@usetada.com>
2021-11-18 00:29:34 +0700

    add case filter line & search

--- cypress/integration/engineering/threshold.js
+++ cypress/integration/engineering/threshold.js
@@ -6,11 +6,11 @@ const url = 'https://api.jeager.io';
 
 describe('Threshold', () => {
   before(() => {
-    cy.clearCookies();
-    cy.clearLocalStorage();
-    cy.mockUserAdmin();
-    cy.login('engineering');
-    cy.get('[title="Threshold"] > a', timeout).click();
+    // cy.login('engineering');
+  });
+
+  beforeEach(() => {
+    cy.visit('/setting-threshold');
   });
 
   describe('Check List Threshold', () => {
@@ -25,6 +25,37 @@ describe('Threshold', () => {
       cy.get('[title="Heat Map"] > a', timeout).click();
       cy.get('[title="Threshold"] > a', timeout).click();
       cy.contains('THRESHOLD', timeout).should('be.visible');
+    });
+  });
+
+  describe('Filter Line', () => {
+    it('Production Line 2', () => {
+      cy.get('.ant-select-selection-item', timeout).click();
+      cy.contains('Production Line 2', timeout).click();
+      cy.contains('1. ac Produksi Aktual Per Detik', timeout).should('be.visible');
+      cy.contains('4. Agromon', timeout).should('be.visible');
+      cy.contains('9. Arus Agitator Ball 2', timeout).should('be.visible');
+    });
+
+    it('Production Line 1', () => {
+      cy.get('.ant-select-selection-item', timeout).click();
+      cy.contains('Production Line 1', timeout).click();
+      cy.contains('1. ac Produksi Aktual Per Detik', timeout).should('be.visible');
+      cy.contains('4. Agromon', timeout).should('be.visible');
+      cy.contains('9. Arus Agitator Ball 2', timeout).should('be.visible');
+    });
+
+  });
+
+  describe('Search', () => {
+    it('Not Found data', () => {
+      cy.get('.ant-input-affix-wrapper', timeout).type('Random');
+      cy.get('.ant-col-23 > form', timeout).contains('Random').should('not.exist');
+    });
+
+    it('Found Data', () => {
+      cy.get('.ant-input-affix-wrapper', timeout).type('Baking Time');
+      cy.get('.ant-col-23 > form', timeout).contains('1. Baking Time').should('be.visible');
     });
   });
 });
