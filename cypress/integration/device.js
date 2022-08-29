f61977c7511c28adfadffbcb0641adb6b71a33bf
viadwimr <viadwimr@github.com>
2022-08-29 13:00:29 +0700

    update device test

--- cypress/integration/device.js
+++ cypress/integration/device.js
@@ -41,12 +41,15 @@ describe('Device', () => {
       cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout)
         .click({force:true});
       cy.contains('Semua Sektor', timeout).should('be.visible');
-      cy.contains('auto trial', timeout).should('be.visible');
-      cy.contains('Ball Mill', timeout).should('be.visible');
-      cy.contains('Ball Mill Line 2', timeout).should('be.visible');
-      cy.contains('Enrober', timeout).should('be.visible');
-      cy.contains('Forming Baking', timeout).should('be.visible');
-      cy.contains('Forming Baking Line 2', timeout).should('be.visible');
+      cy.get(':nth-child(1) > a > .sector-card', timeout).contains('ac Produksi Aktual Per Detik');
+      cy.get(':nth-child(2) > a > .sector-card', timeout).contains('ac Produksi Recipe Per Detik');
+      cy.get(':nth-child(3) > a > .sector-card', timeout).contains('ac Produksi Recipe Per Menit');
+      cy.get(':nth-child(4) > a > .sector-card', timeout).contains('Agromon');
+      cy.get(':nth-child(5) > a > .sector-card', timeout).contains('air blower c');
+      cy.get(':nth-child(6) > a > .sector-card', timeout).contains('air blower p');
+      cy.get(':nth-child(7) > a > .sector-card', timeout).contains('Antares Perangkat');
+      cy.get(':nth-child(8) > a > .sector-card', timeout).contains('Antares Perangkat 1');
+      cy.get(':nth-child(9) > a > .sector-card', timeout).contains('Antares perangkat 2');
     });
     /*
     it('Sorting List Filter Sektor di Form', () => {
@@ -66,20 +69,21 @@ describe('Device', () => {
     it('Sorting List Sektor di Tabel', () => {
       cy.wait(3000);
       cy.get('[data-testid=atur-sector-button]', timeout).click();
-      cy.get('[data-row-key="617a5f86af2eb10001802249"] > :nth-child(1)', timeout)
-        .contains('auto trial').should('be.visible');
+      cy.get('[data-row-key="61dfc15e3fd9cf00013a335c"] > :nth-child(1)', timeout)
+        .contains('Antares Power Monitoring');
+      cy.get('[data-row-key="61efb8bdbb9f91000183d6e6"] > :nth-child(1)', timeout)
+        .contains('Baking Line 2');
       cy.get('[data-row-key="6020f89178b2e30001e0cff4"] > :nth-child(1)', timeout)
-        .should('have.contain', 'Ball Mill');
+        .contains('Ball Mill');
       cy.get('[data-row-key="616efef25493c40001803758"] > :nth-child(1)', timeout)
-        .should('have.contain', 'Ball Mill Line 2');
+        .contains('Ball Mill Line 2');
       cy.get('[data-row-key="6020f89178b2e30001e0cff5"] > :nth-child(1)', timeout)
-        .should('have.contain', 'Enrober');
-      cy.get('[data-row-key="60138417516ed50001e90948"] > :nth-child(1)', timeout)
-        .should('have.contain', 'Forming Baking');
+        .contains('Enrober');
     });
   });
-  
+
   describe('Detail Device', () => {
+    // bug tombol simpan tidak bisa diklik
     /*
     describe('Edit Device', () => {
       it('Simpan', () => {
@@ -87,7 +91,8 @@ describe('Device', () => {
         cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
         cy.contains('Ball Mill', timeout).click();
         cy.get(':nth-child(1) > a > .sector-card', timeout).click();
-        cy.get('[style="margin-left: -5px; margin-right: -5px; row-gap: 0px;"] > :nth-child(1) > .Button__BaseButton-sc-1hmbtsr-0', timeout).click({multiple:true});
+        cy.wait(3000);
+        cy.get('[style="margin-left: -5px; margin-right: -5px;"] > :nth-child(1) > .Button__BaseButton-sc-1hmbtsr-0', timeout).click({multiple:true});
         cy.get('[data-testid=input-device_name]', timeout).clear().type('ac per detik');
         cy.get('[data-testid=select-sector] > .ant-select-selector', timeout).click();
         cy.contains('Mixer', timeout).click();
@@ -101,7 +106,7 @@ describe('Device', () => {
         cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
         cy.contains('Mixer', timeout).click();
         cy.get(':nth-child(1) > a > .sector-card', timeout).click();
-        cy.get('[style="margin-left: -5px; margin-right: -5px; row-gap: 0px;"] > :nth-child(1) > .Button__BaseButton-sc-1hmbtsr-0', timeout).click({multiple:true});
+        cy.get('[style="margin-left: -5px; margin-right: -5px;"] > :nth-child(1) > .Button__BaseButton-sc-1hmbtsr-0', timeout).click({multiple:true});
         cy.get('[data-testid=select-sector] > .ant-select-selector', timeout).click();
         cy.contains('Ball Mill', timeout).click();
         cy.get('[style="margin-bottom: 198px;"] > .ant-btn', timeout).click();
@@ -121,7 +126,7 @@ describe('Device', () => {
         cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
         cy.contains('Ball Mill', timeout).click();
         cy.get(':nth-child(1) > a > .sector-card', timeout).click();
-        cy.get('[style="margin-left: -5px; margin-right: -5px; row-gap: 0px;"] > :nth-child(1) > .Button__BaseButton-sc-1hmbtsr-0', timeout).click({multiple:true});
+        cy.get('[style="margin-left: -5px; margin-right: -5px;"] > :nth-child(1) > .Button__BaseButton-sc-1hmbtsr-0', timeout).click({multiple:true});
         cy.get('[style="margin-bottom: 198px;"] > .ant-btn', timeout).click();
         cy.get(':nth-child(6) > .ant-row > .ant-col > .ant-form-item-control-input > .ant-form-item-control-input-content > [data-testid=input-additional_info]', timeout).clear({force:true}).type('auto update', {force:true});
         cy.get(':nth-child(8) > .ant-row > .ant-col > .ant-form-item-control-input > .ant-form-item-control-input-content > [data-testid=input-additional_info]', timeout).clear({force:true}).type('auto 2 update', {force:true});
@@ -134,7 +139,7 @@ describe('Device', () => {
         cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
         cy.contains('Ball Mill', timeout).click();
         cy.get(':nth-child(1) > a > .sector-card', timeout).click();
-        cy.get('[style="margin-left: -5px; margin-right: -5px; row-gap: 0px;"] > :nth-child(1) > .Button__BaseButton-sc-1hmbtsr-0', timeout).click({multiple:true});
+        cy.get('[style="margin-left: -5px; margin-right: -5px;"] > :nth-child(1) > .Button__BaseButton-sc-1hmbtsr-0', timeout).click({multiple:true});
         cy.get('[data-testid=input-device_name]', timeout).should('have.value', 'ac per detik');
         cy.get('[data-testid=select-sector] > .ant-select-selector', timeout).should('have.contain', 'Ball Mill');
         cy.get('[data-testid=input-location]', timeout).should('have.value', 'auto test');
@@ -148,7 +153,7 @@ describe('Device', () => {
         cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
         cy.contains('Ball Mill', timeout).click();
         cy.get(':nth-child(1) > a > .sector-card', timeout).click();
-        cy.get('[style="margin-left: -5px; margin-right: -5px; row-gap: 0px;"] > :nth-child(1) > .Button__BaseButton-sc-1hmbtsr-0', timeout).click({multiple:true});
+        cy.get('[style="margin-left: -5px; margin-right: -5px;"] > :nth-child(1) > .Button__BaseButton-sc-1hmbtsr-0', timeout).click({multiple:true});
         cy.get(':nth-child(6) > [style="height: 40px; display: flex; align-items: center;"]', timeout).click();
         cy.contains('Hapus').click({force:true});
         cy.get(':nth-child(6) > [style="height: 40px; display: flex; align-items: center;"]', timeout).click();
@@ -158,6 +163,7 @@ describe('Device', () => {
       });
     });
     */
+    // bug tombol simpan tidak bisa diklik
     /*
     describe('Setting Kalibrasi', () => {
       it('Validasi Input dan Simpan', () => {
@@ -193,9 +199,14 @@ describe('Device', () => {
         cy.wait(3000);
         cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
         cy.contains('Ball Mill', timeout).click();
+        cy.wait(3000);
         cy.get(':nth-child(1) > a > .sector-card', timeout).click();
-        cy.get(':nth-child(2) > .ant-row > :nth-child(2) > .ant-dropdown-trigger', timeout).click();
+        cy.wait(3000);
+        cy.get(':nth-child(2) > .ant-row > :nth-child(2) > .ant-dropdown-trigger > svg', timeout).click();
         cy.contains('Unduh sebagai JPG', timeout).click({force:true});
+        cy.server().should((server) => {
+          expect(server.status).to.eq(200);
+        });  
       });
       
       it('Export PDF', () => {
@@ -203,8 +214,12 @@ describe('Device', () => {
         cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
         cy.contains('Ball Mill', timeout).click();
         cy.get(':nth-child(1) > a > .sector-card', timeout).click();
-        cy.get(':nth-child(2) > .ant-row > :nth-child(2) > .ant-dropdown-trigger', timeout).click();
+        cy.wait(3000);
+        cy.get(':nth-child(2) > .ant-row > :nth-child(2) > .ant-dropdown-trigger > svg', timeout).click();
         cy.contains('Unduh sebagai PDF', timeout).click({force:true});
+        cy.server().should((server) => {
+          expect(server.status).to.eq(200);
+        });  
       });
     });
     
@@ -212,28 +227,28 @@ describe('Device', () => {
       it('Read and Update', () => {
         cy.wait(3000);
         cy.get('[data-testid=select-sector] > .ant-select-selector > .ant-select-selection-item', timeout).click({force:true});
-        cy.contains('Forming Line 2', timeout).click();
-        cy.get(':nth-child(1) > a > .sector-card', timeout).click();
-        cy.get(':nth-child(1) > .ant-card-body > .reason-card__container--warning > .reason-card__title--warning', timeout)
-          .contains('CPM - Percent').should('be.visible');
-        cy.get(':nth-child(1) > .ant-card-body > .reason-card__container--warning > :nth-child(2)', timeout)
-          .contains('3/12/2021').should('be.visible');
-        cy.get(':nth-child(1) > .ant-card-body > .reason-card__container--warning > :nth-child(3)', timeout)
-          .contains('14:32:48 - 14:42:48').should('be.visible');
+        cy.contains('Mixer', timeout).click();
+        cy.get(':nth-child(4) > a > .sector-card', timeout).click();
+        cy.get(':nth-child(4) > .ant-card-body > .reason-card__container--warning > .reason-card__title--warning', timeout)
+          .contains('Arus Agitator Ball 2 - Arus Listrik').should('be.visible');
+        cy.get(':nth-child(4) > .ant-card-body > .reason-card__container--warning > :nth-child(2)', timeout)
+          .contains('28/08/2022').should('be.visible');
+        cy.get(':nth-child(4) > .ant-card-body > .reason-card__container--warning > :nth-child(3)', timeout)
+          .contains('19:03:26 - 20:05:13').should('be.visible');
         cy.get(':nth-child(3) > .ant-card-body > .reason-card__container--warning', timeout)
           .click();
-        cy.get('#input_reason_reason', timeout).type('auto tes cpm');
+        cy.get('#input_reason_reason', timeout).clear().type('auto tes aab 2');
         cy.contains('Submit', timeout).click();
         cy.contains('Data berhasil diperbaharui', timeout).should('be.visible');
         cy.get(':nth-child(3) > .ant-card-body > .reason-card__container--warning', timeout)
           .click();
         cy.get(':nth-child(2) > .input-reason-form__row__field', timeout)
-          .should('have.contain', 'CPM - Percent');
-        cy.get('[style="margin-left: -12px; margin-right: -12px; row-gap: 0px;"] > :nth-child(1) > .input-reason-form__row > .input-reason-form__row__field', timeout)
-          .should('have.contain', '15.41.52');
+          .should('have.contain', 'Arus Agitator Ball 2 - Arus Listrik');
+        cy.get('[style="margin-left: -12px; margin-right: -12px;"] > :nth-child(1) > .input-reason-form__row > .input-reason-form__row__field', timeout)
+          .should('have.contain', '20.05.13');
         cy.get(':nth-child(2) > .input-reason-form__row > .input-reason-form__row__field', timeout)
-          .should('have.contain', '15.55.53');
-        cy.get('#input_reason_reason', timeout).should('have.value', 'auto tes cpm');
+          .should('have.contain', '21.07.21');
+        cy.get('#input_reason_reason', timeout).should('have.value', 'auto tes aab 2');
         cy.get('.ant-modal-close-x', timeout).click();
         cy.get(':nth-child(3) > .ant-card-body > .reason-card__container--warning', timeout)
           .click();
