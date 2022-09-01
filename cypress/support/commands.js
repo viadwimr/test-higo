131823f0b0a0b02154e869242d9301b46022793d
viadwimr <viadwimr@github.com>
2022-09-02 02:05:52 +0700

    update username

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,7 +4,7 @@ var timeout = { timeout: 60000 }
 
 Cypress.Commands.add('login', () => {
   cy.visit('/');
-  cy.get('#username', timeout).type('admin');
+  cy.get('#username', timeout).type('admingf');
   cy.get('#password', timeout).type('password');
   cy.get('.ant-btn', timeout).click();
 });
