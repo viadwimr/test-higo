261affb994625ff17f713d0cce611e91c3f4d01b
viadwimr <viadwimr@github.com>
2022-09-09 14:08:45 +0700

    update akun login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,7 +4,7 @@ var timeout = { timeout: 60000 }
 
 Cypress.Commands.add('login', () => {
   cy.visit('/');
-  cy.get('#username', timeout).type('admingf');
+  cy.get('#username', timeout).type('tes-admin');
   cy.get('#password', timeout).type('password');
   cy.get('.ant-btn', timeout).click();
 });
