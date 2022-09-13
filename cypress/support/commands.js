e99556275b263e48926f27d1d67830a660268593
viadwimr <viadwimr@github.com>
2022-09-13 17:12:17 +0700

    add role account

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,9 +2,21 @@
 
 var timeout = { timeout: 60000 }
 
-Cypress.Commands.add('login', () => {
-  cy.visit('/');
-  cy.get('#username', timeout).type('tes-admin');
-  cy.get('#password', timeout).type('password');
-  cy.get('.ant-btn', timeout).click();
+Cypress.Commands.add('login', (numb) => {
+  if(numb === 'admin') {
+    cy.visit('/');
+    cy.get('#username', timeout).type('tes-admin');
+    cy.get('#password', timeout).type('password');
+    cy.get('.ant-btn', timeout).click();
+  } else if (numb === 'engineering') {
+    cy.visit('/');
+    cy.get('#username', timeout).type('tes-engineering');
+    cy.get('#password', timeout).type('password');
+    cy.get('.ant-btn', timeout).click();
+  } else if (numb === 'operator') {
+    cy.visit('/');
+    cy.get('#username', timeout).type('tes-operator');
+    cy.get('#password', timeout).type('password');
+    cy.get('.ant-btn', timeout).click();
+  }
 });
