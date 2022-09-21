7655bc5ee458d4376caa086d937e25b3241dde49
viadwimr <viadwimr@github.com>
2022-09-21 11:49:26 +0700

    delete engineering account

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,20 +3,25 @@
 var timeout = { timeout: 60000 }
 
 Cypress.Commands.add('login', (numb) => {
+  cy.clearCookies();
+  cy.clearLocalStorage();
+  cy.visit('/');
   if(numb === 'admin') {
-    cy.visit('/');
     cy.get('#username', timeout).type('tes-admin');
     cy.get('#password', timeout).type('password');
     cy.get('.ant-btn', timeout).click();
-  } else if (numb === 'engineering') {
-    cy.visit('/');
-    cy.get('#username', timeout).type('tes-engineering');
-    cy.get('#password', timeout).type('password');
-    cy.get('.ant-btn', timeout).click();
+    cy.contains('Dashboard', timeout).should('be.visible');
+    cy.contains('Device', timeout).should('be.visible');
+    cy.contains('Report', timeout).should('be.visible');
+    cy.contains('User', timeout).should('be.visible');
+    cy.contains('Threshold', timeout).should('be.visible');
   } else if (numb === 'operator') {
-    cy.visit('/');
+    // cy.visit('/');
     cy.get('#username', timeout).type('tes-operator');
     cy.get('#password', timeout).type('password');
     cy.get('.ant-btn', timeout).click();
+    cy.contains('Dashboard', timeout).should('be.visible');
+    cy.contains('Device', timeout).should('be.visible');
+    cy.contains('Report', timeout).should('be.visible');
   }
 });
