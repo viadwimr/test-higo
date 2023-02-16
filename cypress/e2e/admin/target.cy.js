5dc2eb11d6787790befb07ca3bb5bfece0b6d2b2
viadwimr <viadwimr@github.com>
2023-02-16 15:17:18 +0700

    add target test

--- cypress/e2e/admin/target.cy.js
+++ cypress/e2e/admin/target.cy.js
@@ -1,161 +1,91 @@
 /// <reference types="Cypress" />
 
-var timeout = { timeout: 5000 }
+var timeout = { timeout: 60000 }
 
 describe('Target', () => {
   before(() => {
     cy.login('admin');
   });
-  
-  describe('Save Target', () => {
-    beforeEach(() => {
-      cy.visit('/setting-target');
-      cy.wait(3000);
-      // filter sektor
-      cy.get('.ant-select-selection-item', timeout).click();
-      cy.wait(3000);
-      cy.contains('Gedung 2', timeout).click();
-      cy.contains('No data', timeout).should('be.visible');
-      cy.get('.ant-select-selection-item', timeout).click();
-      cy.wait(3000);
-      cy.contains('Gedung 1', timeout).click();
-      // filter device
-      cy.get('form > :nth-child(1) > label', timeout).click({force:true});
-    });
-    
-    describe('Validasi Input', () => {
-      it('Kosong', () => {
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear();
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear();
-        cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).click();
-        cy.get('#target_form_target_index_help > .ant-form-item-explain-error', timeout).contains('Target is required');
-        cy.get('#target_form_target_energy_help > .ant-form-item-explain-error', timeout)
-          .contains('Target is required');
-      });
 
-      it('Input letter', () => {
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('satu');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('dua');
-        cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).click();
-        cy.get('#target_form_target_index_help > .ant-form-item-explain-error', timeout).contains('Target is required');
-        cy.get('#target_form_target_energy_help > .ant-form-item-explain-error', timeout)
-          .contains('Target is required');
-      });
+  it('Akan muncul halaman untuk setting target', () => {
+    cy.get('[title="Target"] > .ant-menu-title-content > a', timeout).click();
+    cy.get('.title', timeout).contains('TARGET');
+  });
 
-      it('Input float', () => {
-        // comma
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('0,2');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('9,8');
-        cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).click();
-        // input comma can't visible
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .should('have.value','2');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .should('have.value','98');
+  it('Menampilkan device berdasarkan pilihan sektor', () => {
+    cy.wait(3000);
+    // list device
+    cy.get('form > :nth-child(1) > label', timeout).contains('MP_01');
+    cy.get('form > :nth-child(2) > label', timeout).contains('Power Meter Trial');
+    // filter sektor
+    cy.get('.ant-select-selection-item', timeout).click();
+    cy.wait(1000);
+    cy.contains('Gedung 1', timeout).click();
+    // list device
+    cy.get('form > :nth-child(1) > label', timeout).contains('Power Meter Trial');
+  });
 
-        // dot
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('0.2');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('9.8');
-        cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).click();
-        // check input
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .should('have.value','0.2');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .should('have.value','9.8');
-      });
-
-      it('Input minus', () => {
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('-1');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('-99');
-        cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).click();
-        // check input
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .should('have.value','-1');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .should('have.value','-99');
-      });
-    });
-
-    describe('CRUD', () => {
-      beforeEach(() => {
-        cy.visit('/setting-target');
-        cy.wait(3000);
-        // filter sektor
-        cy.get('.ant-select-selection-item', timeout).click();
-        cy.wait(3000);
-        cy.contains('Gedung 2', timeout).click();
-        cy.contains('No data', timeout).should('be.visible');
-        cy.get('.ant-select-selection-item', timeout).click();
-        cy.wait(3000);
-        cy.contains('Gedung 1', timeout).click();
-        // filter device
-        cy.get('form > :nth-child(1) > label', timeout).click({force:true});
-      });
-
-      it('Add target', () => {
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('1');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('90000');
-        cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).click();
-      });
-
-      it('Check target', () => {
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .should('have.contain', '1');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-        .should('have.contain', '90000');
-      });
-
-      it('Update target', () => {
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('90000');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-          .clear().type('1');
-        cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).click();
-
-        // check target after update
-        cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-        .should('have.contain', '90000');
-        cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
-        .should('have.contain', '1');
-      });
-    });
+  it('Menampilkan kolom indikator yang ada pada form target', () => {
+    cy.get('form > :nth-child(1) > label', timeout).click({force:true});
+    cy.get('.ant-col-24 > .TargetPage__H5-sc-1qpmm05-1', timeout).contains('Target');
+    cy.get('#target_form_target_energy > .TargetPage__Label-sc-1qpmm05-3', timeout).contains('Energy (kWh)');
+    cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout).should('be.visible');
+    cy.get('#target_form_target_index > .TargetPage__Label-sc-1qpmm05-3', timeout).contains('Index');
+    // bug: unit satuan index belum ditulis
+    cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout).should('be.visible');
+    cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).should('be.visible');
   });
   
-  describe('Search Device', () => {
-    beforeEach(() => {
-      cy.visit('/setting-target');
-      cy.wait(3000);
-      cy.get('.ant-select-selection-item', timeout).click();
-      cy.wait(3000);
-    });
+  it('Data nilai target berhasil diperbarui dan disimpan', () => {
+    cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
+      .clear().type('1');
+    cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
+      .clear().type('90000');
+    cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).click();
 
-    it('Semua Sektor', () => {
-      cy.contains('Semua Sektor', timeout).click();
-      cy.get('.ant-input-affix-wrapper', timeout).clear().type('01');
-      cy.get('form > :nth-child(1) > label', timeout).contains('MP_01');
-      cy.get('form > :nth-child(2) > label', timeout).should('not.exist');
-      cy.contains('Power Meter Trial', timeout).should('not.exist');
-    });
+    // check the result
+    cy.reload();
+    cy.get('.title', timeout).contains('TARGET');
+    // filter sektor
+    cy.wait(3000);
+    cy.get('.ant-select-selection-item', timeout).click();
+    cy.wait(1000);
+    cy.contains('Gedung 1', timeout).click();
+    // filter device
+    cy.get('form > :nth-child(1) > label', timeout).click({force:true});
+    // form target
+    cy.get('.ant-col-24 > .TargetPage__H5-sc-1qpmm05-1', timeout).contains('Target');
+    cy.get('#target_form_target_energy > .TargetPage__Label-sc-1qpmm05-3', timeout).contains('Energy (kWh)');
+    cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout).should('be.visible');
+    cy.get('#target_form_target_index > .TargetPage__Label-sc-1qpmm05-3', timeout).contains('Index');
+    // bug: unit satuan index belum ditulis
+    cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout).should('be.visible');
+    cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).should('be.visible');
+    // check value
+    cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
+    .should('have.value', '1');
+    cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
+    .should('have.value', '90000');
 
-    it('No Device Sektor', () => {
-      cy.contains('Gedung 2', timeout).click();
-      cy.get('.ant-input-affix-wrapper', timeout).should('not.be.enabled');
-      cy.get('form > :nth-child(1) > label', timeout).should('not.exist');
-      cy.get('form > :nth-child(2) > label', timeout).should('not.exist');
-      cy.contains('MP_01', timeout).should('not.exist');
-      cy.contains('Power Meter Trial', timeout).should('not.exist');
-    });
+    // back to prev data
+    cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
+    .clear().type('90000');
+    cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
+      .clear().type('1');
+    cy.get('.TargetPage__BtnSave-sc-1qpmm05-5', timeout).click();
+    // check the result
+    cy.reload();
+    // filter sektor
+    cy.wait(3000);
+    cy.get('.ant-select-selection-item', timeout).click();
+    cy.wait(1000);
+    cy.contains('Gedung 1', timeout).click();
+    // filter device
+    cy.get('form > :nth-child(1) > label', timeout).click({force:true});
+    // check value
+    cy.get('#target_form_target_energy > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
+    .should('have.value', '90000');
+    cy.get('#target_form_target_index > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input', timeout)
+    .should('have.value', '1');
   });
 });
