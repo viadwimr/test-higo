9443ab859d11be396003c7cd4f0931043df7a0b7
viadwimr <viadwimr@github.com>
2023-02-17 10:36:13 +0700

    add sign-out test

---
+++ cypress/e2e/admin/sign-out.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+var timeout = { timeout: 60000 }
+var force = { force: true }
+
+describe('Signout', () => {
+  before(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.login('admin');
+  });
+
+  it('Keluar Aplikasi', () => {
+    cy.get(':nth-child(2) > .ant-dropdown-trigger', timeout).click();
+    cy.wait(1000);
+    cy.contains('Logout', timeout).click();
+    cy.wait(7000);
+    cy.get('#username', timeout).should('be.visible');
+    cy.get('#password', timeout).should('be.visible');
+    cy.get('.ant-btn', timeout).should('be.visible');
+  });
+});
