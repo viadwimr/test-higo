a2324fc9abc2eec702b14b1dc89d82fdf708c344
viadwimr <viadwimr@github.com>
2023-05-03 15:21:55 +0700

    update notif and add validation login test

--- cypress/e2e/admin/login.cy.js
+++ cypress/e2e/admin/login.cy.js
@@ -16,10 +16,32 @@ describe('Login', () => {
   });
 
   it('Jika username dan password yang di-entry valid maka web akan menampilkan Halaman Home / halaman sebelum melakukan login', () => {
+    // wrong password
     cy.get('#username', timeout).type('admindenso');
     cy.get('#password', timeout).type('12334345');
     cy.get('.ant-btn', timeout).click();
-    cy.contains('User not found, username or password is incorrect', timeout).should('be.visible');
+    cy.contains('User Not Found, username or password is incorrect', timeout).should('be.visible');
+    cy.get('.swal2-confirm', timeout).click();
+
+    // long password
+    cy.get('#username', timeout).clear().type('admin-denso');
+    cy.get('#password', timeout).type('Jeager123');
+    cy.get('.ant-btn', timeout).click();
+    cy.contains('Gagal', timeout).should('be.visible');
+    cy.get('.swal2-confirm', timeout).click();
+
+    // wrong username
+    cy.get('#username', timeout).clear().type('admin-denso');
+    cy.get('#password', timeout).clear().type('Jeager123');
+    cy.get('.ant-btn', timeout).click();
+    cy.contains("400 Cannot read property 'clientId' of null", timeout).should('be.visible');
+    cy.get('.swal2-confirm', timeout).click();
+
+    // wrong username & password
+    cy.get('#username', timeout).clear().type('admin-denso');
+    cy.get('#password', timeout).clear().type('12334345');
+    cy.get('.ant-btn', timeout).click();
+    cy.contains('Gagal', timeout).should('be.visible');
     cy.get('.swal2-confirm', timeout).click();
   });
 
@@ -30,12 +52,10 @@ describe('Login', () => {
     cy.get('.logo > img', timeout).should('be.visible');
     cy.contains('Dashboard', timeout).should('be.visible');
     cy.contains('Device', timeout).should('be.visible');
-    cy.contains('Report', timeout).should('be.visible');
+    cy.contains('Laporan', timeout).should('be.visible');
     cy.contains('User', timeout).should('be.visible');
-    cy.contains('Alert', timeout).should('be.visible');
     cy.get('[title="Target"] > .ant-menu-title-content > a', timeout).should('be.visible');
-    cy.contains('Indicator', timeout).should('be.visible');
-    cy.contains('Analysis', timeout).click();
+    cy.contains('Analisis', timeout).click();
     cy.contains('Trend', timeout).should('be.visible');
   });
 });
