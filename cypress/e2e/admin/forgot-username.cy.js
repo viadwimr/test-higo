a651357ad5a439067c085606b7366e9479c7e033
viadwimr <viadwimr@github.com>
2023-05-04 15:40:18 +0700

    update notif and add forgot username validation test

--- cypress/e2e/admin/forgot-username.cy.js
+++ cypress/e2e/admin/forgot-username.cy.js
@@ -16,14 +16,39 @@ describe('Forgot Username', () => {
   })
 
   it('Tombol Kirim akan aktif setelah memasukkan email dan no HP', () => {
-    cy.get('#lupa_username_email', timeout).type('qaevomotelkom@gmail.com');
+    // wrong email
+    cy.get('#lupa_username_email', timeout).type('qa-evomo_telkom@gmail.com');
     cy.get('#lupa_username_no_hp', timeout).type('09876543210');
+    cy.get('.ant-btn-primary', timeout).click();
+    cy.contains('Gagal', timeout).should('be.visible')
+    cy.get('.swal2-confirm', timeout).click();
+
+    // wrong no hp
+    cy.get('#lupa_username_email', timeout).clear().type('qaevomotelkom@gmail.com');
+    cy.get('#lupa_username_no_hp', timeout).clear().type('09876543210123');
+    cy.get('.ant-btn-primary', timeout).click();
+    cy.contains('Gagal', timeout).should('be.visible')
+    cy.get('.swal2-confirm', timeout).click();
+
+    // wrong email dan no hp
+    cy.get('#lupa_username_email', timeout).clear().type('qa-evomo_.telkom@gmail.com');
+    cy.get('#lupa_username_no_hp', timeout).clear().type('09876543210123');
+    cy.get('.ant-btn-primary', timeout).click();
+    cy.contains('Gagal', timeout).should('be.visible')
+    cy.get('.swal2-confirm', timeout).click();
+
+    // valid email dan no hp
+    cy.get('#lupa_username_email', timeout).clear().type('qaevomotelkom@gmail.com');
+    cy.get('#lupa_username_no_hp', timeout).clear().type('09876543210');
     cy.get('.ant-btn-primary', timeout).click();
   })
 
   it('Akan mengirim pemberitahuan username di alamat email yang terdaftar', () => {
-    cy.contains('Success', timeout).should('be.visible');
+    cy.contains('Berhasil', timeout).should('be.visible');
     cy.wait(1000);
+    cy.server().should((server) => {
+      expect(server.status).to.eq(200);
+    });
   })
 });
 
