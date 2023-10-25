65aa0f5ff1d22ed446f397a7b72e4839a255c536
viadwimr <viadwimr@github.com>
2023-10-25 12:41:10 +0700

    add more clear cache

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -5,6 +5,8 @@ import '@shelex/cypress-allure-plugin'
 Cypress.on('uncaught:exception', (err, runnable) => false);
 
 before(function () {
+  cy.clearCookies();
+  cy.clearLocalStorage();
   cy.visit('/');
   Cypress.Cookies.preserveOnce('secret');
 });
